4408896d0d213d6b87541f8c473ef0da982cd281
Ruan Quadros <Ruanengs@gmail.com>
2026-02-13 11:16:14 -0300

    Movimentação de arquivos.

--- projeto-java/src/edu/ruan/primeirasemana/BoletimEstudantil.java
+++ projeto-java/src/edu/ruan/primeirasemana/BoletimEstudantil.java
@@ -1,3 +1,4 @@
+package edu.ruan.primeirasemana;
 public class BoletimEstudantil {
     public static void main(String[] args) {
         int mediaFinal = 3;
--- projeto-java/src/edu/ruan/primeirasemana/anatomia4.java
+++ projeto-java/src/edu/ruan/primeirasemana/anatomia4.java
@@ -1,3 +1,4 @@
+package edu.ruan.primeirasemana;
 public class anatomia4 {
     public static void main(String[] args) {
         int somaUm = 50;
@@ -5,7 +6,6 @@ public class anatomia4 {
         somar(somaUm, somaDois);
         
     }
-
     public static void somar (int a, int b) {
         int resultado = a + b;
         System.out.println("A soma entre x + y é: " + resultado);
