eacc6586b48439d8ac431f5db4eb562598d7f0de
Ruan Quadros <Ruanengs@gmail.com>
2026-02-13 10:49:32 -0300

    Add arquivo

---
+++ projeto-java/src/anatomia4.java
@@ -0,0 +1,13 @@
+public class anatomia4 {
+    public static void main(String[] args) {
+        int somaUm = 50;
+        int somaDois = 50;
+        somar(somaUm, somaDois);
+        
+    }
+
+    public static void somar (int a, int b) {
+        int resultado = a + b;
+        System.out.println("A soma entre x + y é: " + resultado);
+    }
+}
